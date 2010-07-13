08956f5b26ea9ae2460d618d36c1dfeb42381132
evillemez <evillemez@gmail.com>
2010-07-13 17:29:27 -0400

    implementing a a transcoder_plugin interface... not done yet

--- modules/transcoder/theme/admin-transcoder.tpl.php
+++ modules/transcoder/theme/admin-transcoder.tpl.php
@@ -1,13 +1,31 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
 <h2>Transcoding</h2>
 <p>Here are the plugins available:</p>
-<pre><?php print_r($plugins) ?></pre>
-
+<?php if($plugins): ?>
+<ul>
+	<?php foreach($plugins as $plugin): ?>
+	<li><?php print $plugin; ?></li>
+	<?php endforeach; ?>
+</ul>
+<?php endif; ?>
 <p>These are the presets available for use:</p>
-<pre><?php print_r($presets) ?></pre>
+<p><a href=<?php print $agave->base_url."admin/transcoder/preset/create"?>>Create Preset</a></p>
+<?php if($presets): ?>
+	<table>
+		<thead>
+			<th>Name</th>
+			<th>Plugin</th>
+			<th>Accepted Extensions</th>
+			<th></th>
+		</thead>
+		<tbody>
+			<?php foreach($presets as $preset): ?>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+<?php endif; ?>
