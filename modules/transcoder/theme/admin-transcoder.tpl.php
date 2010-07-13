892088a779cddb00b4d6ea4dd26b9f78954398f3
jwarchal <Julian.Warchall@gmail.com>
2010-07-13 13:50:24 -0400

    making admin transcoder preset interface

--- modules/transcoder/theme/admin-transcoder.tpl.php
+++ modules/transcoder/theme/admin-transcoder.tpl.php
@@ -6,8 +6,10 @@
 
 ?>
 <h2>Transcoding</h2>
-<p>Here are the plugins available:</p>
-<pre><?php print_r($plugins) ?></pre>
-
-<p>These are the presets available for use:</p>
-<pre><?php print_r($presets) ?></pre>
+<p>Following are the transcoding plugins available.  Click a plugin's name to view and create presets for it.</p>
+<pre><?php
+global $agave;
+foreach($plugins as $name=>$path) { 
+    print('<a href="'.$agave->base_url.'admin/transcoder/presets&name='.$name.'"> - '.$name.'</a><br/><br/>');
+}
+?></pre>
