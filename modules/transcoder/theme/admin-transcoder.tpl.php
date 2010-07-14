49aa6d9d0eaa89e81665b6d8873ad816e10d5022
jwarchal <Julian.Warchall@gmail.com>
2010-07-14 09:57:28 -0400

    changed admin-transcoder tpl file to match new transcoder->getPlugin

--- modules/transcoder/theme/admin-transcoder.tpl.php
+++ modules/transcoder/theme/admin-transcoder.tpl.php
@@ -9,7 +9,7 @@
 <p>Following are the transcoding plugins available.  Click a plugin's name to view and create presets for it.</p>
 <pre><?php
 global $agave;
-foreach($plugins as $name=>$path) { 
+foreach($plugins as $num=>$name) {
     print('<a href="'.$agave->base_url.'admin/transcoder/presets&name='.$name.'"> - '.$name.'</a><br/><br/>');
 }
 ?></pre>
