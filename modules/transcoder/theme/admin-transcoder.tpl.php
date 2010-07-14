d185bd365561cfcda988bdea9ee77d14519265da
jwarchal <Julian.Warchall@gmail.com>
2010-07-14 10:30:19 -0400

    transcoder work

--- modules/transcoder/theme/admin-transcoder.tpl.php
+++ modules/transcoder/theme/admin-transcoder.tpl.php
@@ -1,4 +1,5 @@
 <h2>Transcoding</h2>
+<p><?php print('<a href="'.$agave->base_url.'admin/transcoder/transcode"><strong>Click here to test out the transcoder.</strong></a>'); ?></p>
 <p>Following are the transcoding plugins available.  Click a plugin's name to view and create presets for it.</p>
 <pre><?php
 global $agave;
